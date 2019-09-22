34320df89ee2b95385fd22f5ebba42be070a950b
swonb <swonbinca@gmail.com>
2019-09-22 17:05:29 -0400

    End of Tutorial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers/index.js";
+import BoxCon from "./container/box-container";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(reducers, applyMiddleware(thunk));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  render() {
+    return <BoxCon></BoxCon>;
+  }
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
